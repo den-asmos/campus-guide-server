8a0838fed2720d8ec86f074527aaced72455be08
Valeryia Bareisha <v.bareisha@dvcn.ru>
2026-06-06 13:27:17 +0300

    fix: allowed avatar image formats

--- src/middlewares/image-upload.middleware.ts
+++ src/middlewares/image-upload.middleware.ts
@@ -3,32 +3,32 @@ import multer from "multer";
 import { AppError } from "../errors/app-error";
 
 const MAX_FILE_SIZE = 5 * 1024 * 1024;
-const ALLOWED_MIME_TYPES = ["image/png", "image/jpg", "image/webp"];
+const ALLOWED_MIME_TYPES = ["image/png", "image/jpg", "image/jpeg", "image/webp"];
 
 const storage = multer.memoryStorage();
 
 const fileFilter = (
-	req: Request,
-	file: Express.Multer.File,
-	callback: multer.FileFilterCallback,
+  req: Request,
+  file: Express.Multer.File,
+  callback: multer.FileFilterCallback,
 ) => {
-	if (ALLOWED_MIME_TYPES.includes(file.mimetype)) {
-		callback(null, true);
-	} else {
-		callback(
-			new AppError(
-				"Некорректный формат файла (поддерживаются .png, .jpg и .webp",
-				400,
-				"INVALID_FILE",
-			),
-		);
-	}
+  if (ALLOWED_MIME_TYPES.includes(file.mimetype)) {
+    callback(null, true);
+  } else {
+    callback(
+      new AppError(
+        "Некорректный формат файла (поддерживаются .png, .jpg, .jpeg и .webp",
+        400,
+        "INVALID_FILE",
+      ),
+    );
+  }
 };
 
 export const upload = multer({
-	storage,
-	fileFilter,
-	limits: {
-		fileSize: MAX_FILE_SIZE,
-	},
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: MAX_FILE_SIZE,
+  },
 });
